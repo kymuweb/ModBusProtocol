f61d4ca3831f50a4be96c2073deef120b9a97539
Javier Garcia <kymuweb@gmail.com>
2017-02-08 13:32:48 +0000

    ModBusProtocol
    Modbus TCP, Modbus UDP and Modbus RTU client/server library for .NET

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using EasyModbus;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModbusClientApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ModbusClient modbusClient = new ModbusClient("10.0.2.134", 502);
+            modbusClient.Connect();
+
+            bool[] readCoils = modbusClient.ReadCoils(196, 1);
+            int[] readHoldingRegisters = modbusClient.ReadHoldingRegisters(196, 1);
+
+            for (int i = 0; i < readCoils.Length; i++)
+                Console.WriteLine("Value of Coil " + (9 + i + 1) + " " + readCoils[i].ToString());
+
+            for (int i = 0; i < readHoldingRegisters.Length; i++)
+                Console.WriteLine("Value for HoldingRegister " + (i + 1) + " " + readHoldingRegisters[i].ToString());
+
+            // TODO: Logic will go here
+
+            Console.Write("Press any key to continue ...");
+            Console.ReadKey(true);
+
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ Readme.txt
@@ -0,0 +1,4 @@
+More info. under:
+
+https://sourceforge.net/projects/easymodbustcp/?source=typ_redirect
+http://easymodbustcp.net/
