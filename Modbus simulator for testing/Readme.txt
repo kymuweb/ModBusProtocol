e92725c0224aa62a380d5624131527bfb7dd928e
Javier Garcia <kymuweb@gmail.com>
2017-02-15 12:31:01 +0000

    Modbus simulator added
    Modbus simulator added for code testing

---
+++ Modbus simulator for testing/Readme.txt
@@ -0,0 +1,7 @@
+You must use the local IP: 10.128.1.27 to connect to the simulator.
+
+Notice the 'Address' column in the mod_RSsim is coming with a missing digit, for example, the 
+address 40191 will be 400191 in reality. You can check the title of the message box 'Edit 
+decimal value at XXXXX'. For the title of the dialog the address is fine.
+
+For more info. about the simulator, check: http://www.plcsimulator.org
--- Program.cs
+++ Program.cs
@@ -11,17 +11,17 @@ namespace ModbusClientApplication
     {
         static void Main(string[] args)
         {
-            ModbusClient modbusClient = new ModbusClient("10.0.2.134", 502);
+            ModbusClient modbusClient = new ModbusClient("10.128.1.27", 502);
             modbusClient.Connect();
 
-            bool[] readCoils = modbusClient.ReadCoils(196, 1);
-            int[] readHoldingRegisters = modbusClient.ReadHoldingRegisters(196, 1);
+            bool[] readCoils = modbusClient.ReadCoils(196, 1); // Coil 400197
+            int[] readHoldingRegisters = modbusClient.ReadHoldingRegisters(196, 1); // Address 400197
 
             for (int i = 0; i < readCoils.Length; i++)
-                Console.WriteLine("Value of Coil " + (9 + i + 1) + " " + readCoils[i].ToString());
+                Console.WriteLine("Value of Coil " + readCoils[i].ToString());
 
             for (int i = 0; i < readHoldingRegisters.Length; i++)
-                Console.WriteLine("Value for HoldingRegister " + (i + 1) + " " + readHoldingRegisters[i].ToString());
+                Console.WriteLine("Value for HoldingRegister " + readHoldingRegisters[i].ToString());
 
             // TODO: Logic will go here
 
